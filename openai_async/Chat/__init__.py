e1754bc38856820d4a7d6889a295953aeea5f437
sudoskys <coldlando@hotmail.com>
2022-12-10 21:13:18 +0800

    修复小问题

--- openai_async/Chat/__init__.py
+++ openai_async/Chat/__init__.py
@@ -13,12 +13,7 @@ import re
 
 # import loguru
 # import jiagu
-from snownlp import SnowNLP
-from sumy.nlp.stemmers import Stemmer
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-from sumy.utils import get_stop_words
+
 
 # 基于 Completion 上层
 from openai_async import Completion
@@ -67,7 +62,19 @@ class Talk(object):
         return int(num_chinese * 2 + num_non_chinese * 0.25) + 5
 
     @staticmethod
-    def normal_cut_sentence(text):
+    def english_sentence_cut(text) -> list:
+        list_ = list()
+        for s_str in text.split('.'):
+            if '?' in s_str:
+                list_.extend(s_str.split('?'))
+            elif '!' in s_str:
+                list_.extend(s_str.split('!'))
+            else:
+                list_.append(s_str)
+        return list_
+
+    @staticmethod
+    def chinese_sentence_cut(text) -> list:
         text = re.sub('([。！？\?])([^’”])', r'\1\n\2', text)
         # 普通断句符号且后面没有引号
         text = re.sub('(\.{6})([^’”])', r'\1\n\2', text)
@@ -89,7 +96,7 @@ class Talk(object):
             for j in range(index, start):
                 temp += text[j]
             if temp != '':
-                temp_list = self.normal_cut_sentence(temp)
+                temp_list = self.chinese_sentence_cut(temp)
                 listr += temp_list
             temp = ''
             for k in range(start, end):
@@ -112,8 +119,8 @@ class Talk(object):
         if language == "chinese":
             _reply_list = self.cut_chinese_sentence(sentence)
         else:
-            from nltk.tokenize import sent_tokenize
-            _reply_list = sent_tokenize(sentence, language=language)
+            # from nltk.tokenize import sent_tokenize
+            _reply_list = self.english_sentence_cut(sentence)
         if len(_reply_list) < 1:
             return [sentence]
         return _reply_list
@@ -135,72 +142,6 @@ class Talk(object):
             _prompt_list.append(f"{_head}{item.strip()}")
         _prompt_list = list(filter(None, _prompt_list))
         return _prompt_list
-
-    @staticmethod
-    def summary_v2(sentence, n):
-        # 差缺中文系统
-        LANGUAGE = "english"
-        # 统计中文字符数量
-        if len([c for c in sentence if ord(c) > 127]) / len(sentence) > 0.5:
-            _chinese = True
-            LANGUAGE = "chinese"
-        parser = PlaintextParser.from_string(sentence, Tokenizer(LANGUAGE))
-        stemmer = Stemmer(LANGUAGE)
-        summarizer = Summarizer(stemmer)
-        summarizer.stop_words = get_stop_words(LANGUAGE)
-        _words = summarizer(parser.document, n)
-        _words = [str(i) for i in _words]
-        _return = "".join(_words)
-        return _return
-
-    @staticmethod
-    def summary(sentence, n):
-        """
-        :param sentence: 字符串
-        :param n: 几句话
-        :return: 总结
-        """
-        # 差缺中文系统
-        _chinese = False
-        # 统计中文字符数量
-        if len([c for c in sentence if ord(c) > 127]) / len(sentence) > 0.5:
-            _chinese = True
-        if _chinese:
-            try:
-                s = SnowNLP(sentence)  # str为之前去掉符号的中文字符串
-                _sum = (s.summary(round(n)))  # 进行总结 summary
-                # _sum = jiagu.summarize(sentence, round(n / 10))
-            except Exception as e:
-                _sum = [sentence]
-            content = ",".join(_sum)  # 摘要
-        else:
-            import nltk
-            nltk.download('punkt')
-            nltk.download('stopwords')
-            tokens = nltk.word_tokenize(sentence)
-            # 分句
-            sentences = nltk.sent_tokenize(sentence)
-            # 计算词频
-            frequencies = nltk.FreqDist(tokens)
-            # 计算每个句子的得分
-            scores = {}
-            for sentence_ in sentences:
-                score = 0
-                for word in nltk.word_tokenize(sentence_):
-                    if word in frequencies:
-                        score += frequencies[word]
-                scores[sentence_] = score
-            # 按照得分顺序排序句子
-            sorted_sentences = sorted(scores.items(), key=lambda x: x[1], reverse=True)
-            # 返回前 num_sentences 个句子
-            return_num = round(n / 2)
-            if len(sorted_sentences) < return_num:
-                return_num = len(sorted_sentences)
-            _list = [sentence_[0] for sentence_ in sorted_sentences[:return_num]]
-            content = ",".join(_list)
-        if len(content.strip(" ")) == 0:
-            content = sentence
-        return content
 
 
 class Chatbot(object):
