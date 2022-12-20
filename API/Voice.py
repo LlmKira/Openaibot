82293a3f369c2ac863f6be5a0233403a5a9c70db
James Sky <ltdoge@ltdoge.top>
2022-12-20 11:43:19 +0800

    适配最新最热service.json

--- API/Voice.py
+++ API/Voice.py
@@ -4,21 +4,29 @@ from utils.Data import TTS_REQ, TTS_Clint, Service_Data
 from openai_async.utils.Talk import Talk
 from fastapi.responses import Response
 
+serviceCfg = Service_Data.get_key('./Config/service.json')
+ttsConf = serviceCfg['tts']
+
 class VITS:
-    def get(text, task:int = 1, doReturnRawAudio: bool = True):
-        serviceCfg = Service_Data.get_key('./Config/service.json')
-        ttsConf = serviceCfg['tts']
+    def vits(text, task:int = 1, doReturnRawAudio: bool = True):
+        vitsConf = ttsConf['vits']
         if(Talk.get_language(text) != 'chinese'):
             return False
         newtext = '[ZH]' + text + '[ZH]'
-        reqbody = TTS_REQ(model_name = ttsConf['model_name'],
+        reqbody = TTS_REQ(model_name = vitsConf['model_name'],
                           task_id = task,
                           text = newtext,
-                          speaker_id = ttsConf['speaker_id'])
-        data = TTS_Clint.request_tts_server(url = ttsConf['api'], params = reqbody)
+                          speaker_id = vitsConf['speaker_id'])
+        data = TTS_Clint.request_tts_server(url = vitsConf['api'], params = reqbody)
         if(not data):
             return False
         if(doReturnRawAudio):
             return Response(content = TTS_Clint.decode_wav(data['audio']), media_type = 'audio/x-pcm')
         else:
             return {'success': True, 'response': data['audio'], 'text': text}
+    def get(self, text, task:int = 1, doReturnRawAudio:bool = True):
+        ttsType = ttsConf['type']
+        if(ttsType == 'vits'):
+            return VITS().vits(text, task, doReturnRawAudio)
+        else:
+            return {}  #留个空
