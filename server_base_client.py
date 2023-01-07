ef806045ec625e3c4154e9e520844aa4036f8bc8
sudoskys <coldlando@hotmail.com>
2023-01-07 15:24:04 +0800

    feat:工具库的网络server

--- server_base_client.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 1/7/23 12:29 PM
-# @FileName: run_server.py
-# @Software: PyCharm
-# @Github    ：sudoskys
-import uvicorn
-
-if __name__ == '__main__':
-    uvicorn.run('App.Server:app', host='127.0.0.1', port=9557, reload=True, log_level="debug", workers=1)
