3003ed1c8c5515cb514af508dc3d3a400d73cb6d
sudoskys <coldlando@hotmail.com>
2023-01-16 14:35:48 +0800

    feat:input

---
+++ Assistants/utils/rec.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 1/16/23 2:34 PM
+# @FileName: rec.py
+# @Software: PyCharm
+# @Github    ：sudoskys
+
+class Rec(object):
+
+    def porcupine(self):
+        pass
