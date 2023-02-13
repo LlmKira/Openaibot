5d61165fd0ec5d65c7e2c29307289e831bc1ac75
sudoskys <coldlando@hotmail.com>
2023-02-13 11:44:33 +0800

    feat:增加重试次数和间距

--- App/Event.py
+++ App/Event.py
@@ -71,6 +71,9 @@ if PROXY_CONF.status:
 
 llm_kira.setting.redisSetting = llm_kira.setting.RedisConfig(**REDIS_CONF)
 llm_kira.setting.llmRetryTime = 2
+llm_kira.setting.llmRetryTimeMax = 100
+llm_kira.setting.llmRetryTimeMin = 5
+llm_kira.setting.llmRetryAttempt = 5
 
 urlForm = {
     "Danger.form": [
