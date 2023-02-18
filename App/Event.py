1c1fcbfc88a14256627216b99ab40d4e4e1ad00b
sudoskys <coldlando@hotmail.com>
2023-02-18 20:25:33 +0800

    fix

--- App/Event.py
+++ App/Event.py
@@ -629,7 +629,7 @@ async def Group(Message: User_Message, bot_profile: ProfileReturn, config) -> Pu
     _think = ThinkEngine(profile=conversation)
     _description = str(time.strftime("%Y/%m/%d %H:%M", time.localtime()))
     _description += f" 🌙" if _think.is_night else random.choice([" 🌻", " 🌤", " 🌦"])
-    _description += f"\n{''.join(_think.build_status(rank=30))}"
+    _description += f"\n{''.join(_think.build_status(rank=18))}"
     _description += f"\n📱💬 with {start_name}|"
 
     promptManager = llm_kira.creator.PromptEngine(profile=conversation,
