462bdb18997356d2b4405f50def803b3d3f7f17d
sudoskys <coldlando@hotmail.com>
2023-03-02 20:14:16 +0800

    feat:vits speed up

--- App/Event.py
+++ App/Event.py
@@ -187,7 +187,7 @@ async def TTSSupportCheck(text, user_id, limit: bool = True):
         # 合成
         _spell = [f"{cn[x]}{x}{cn[x]}" for x in cn.keys()]
         _new_text = "".join(_spell)
-        _new_text = "[LENGTH]1.3[LENGTH]" + _new_text
+        _new_text = "[LENGTH]1.4[LENGTH]" + _new_text
         # 接受数据
         result, e = await TTS_Clint.request_vits_server(url=_vits_config["api"],
                                                         params=TTS_REQ(task_id=user_id,
