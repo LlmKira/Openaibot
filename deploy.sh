55102f7bd7764ebba339cd099d1cff37567932b0
sudoskys <coldlando@hotmail.com>
2023-09-27 00:50:44 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,10 +15,18 @@ fi
 echo "$(tput setaf 2)Openaibot directory check complete.$(tput sgr0)"
 
 read -r -p "$(tput setaf 6)This script will install Docker, Redis, RabbitMQ, Node.js, NPM, PM2, and the Openaibot project. THAT ACTION MAY BREAK YOUR SYSTEM. Do you want to proceed? (y/n):$(tput sgr0) " choice
-if [[ ! $choice =~ ^[Yy]$ ]]; then
+case "$choice" in
+[yY][eE][sS] | [yY])
+  ;;
+[nN][oO] | [nN])
   echo "$(tput setaf 1)Installation cancelled.$(tput sgr0)"
   exit 0
-fi
+  ;;
+*)
+  echo "invalid..."
+  exit 1
+  ;;
+esac
 
 # Function to handle errors and exit
 handle_error() {
--- test/test.sh
+++ test/test.sh
@@ -1,2 +1,5 @@
-
-echo -e "\e[92mInstallation completed successfully.\e[0m"
+read -r -p "$(tput setaf 6)This script will install Docker, Redis, RabbitMQ, Node.js, NPM, PM2, and the Openaibot project. THAT ACTION MAY BREAK YOUR SYSTEM. Do you want to proceed? (y/n):$(tput sgr0) " rrchoice
+if [[ ! $rrchoice =~ ^[Yy]$ ]]; then
+  echo "$(tput setaf 1)Installation cancelled.$(tput sgr0)"
+  exit 0
+fi
