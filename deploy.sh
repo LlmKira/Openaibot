47fe96d8279a86d1fc5aee89e57eb418fa1a2f4d
sudoskys <coldlando@hotmail.com>
2023-09-27 00:34:08 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ if [ -d "Openaibot" ]; then
   git pull || echo "Failed,pls update it by yourself."
   echo "$(tput setaf 6)Update successfully...$(tput sgr0)"
   exit 0
+  exit 0
 else
   # Clone the project if not already cloned
   git clone https://github.com/LlmKira/Openaibot.git
