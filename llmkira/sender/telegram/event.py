bb531447cd961e395370de9409bd1137bbd4a795
sudoskys <coldlando@hotmail.com>
2023-10-15 00:51:03 +0800

    add help_message()

--- llmkira/sender/telegram/event.py
+++ llmkira/sender/telegram/event.py
@@ -10,9 +10,11 @@ def help_message():
     /chat - 聊天
     /task - 任务
     /tool - 工具列表
+    /clear - 删除自己的记录
+    
+Private Chat Only:
     /bind - 绑定可选平台
     /unbind - 解绑可选平台
-    /clear - 删除自己的记录
     /rset_endpoint - 自定义后端
     /rset_key - 设置openai
     /clear_rset - 抹除自定义设置
