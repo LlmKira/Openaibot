0ff12db0c7e14e477c3d701aea5fd5d6ab7626c8
sudoskys <coldlando@hotmail.com>
2023-11-08 17:40:50 +0800

    :hammer: chore(llmkira): refactor schema.py
    Refactored the schema.py file in the llmkira/sdk directory. Updated
    imports and added type annotations. Cleaned up code and removed unused
    functions. Improved code readability and maintainability.

--- llmkira/sdk/schema.py
+++ llmkira/sdk/schema.py
@@ -6,7 +6,7 @@
 import asyncio
 import time
 # ATTENTION:禁止调用上层任何schema包，否则会导致循环引用
-from typing import Literal, Optional, Coroutine, List
+from typing import Literal, Optional, Coroutine, List, Type
 
 import shortuuid
 from loguru import logger
@@ -57,7 +57,7 @@ class Function(BaseModel):
 
     def request_final(self):
         """
-        生成参数
+        标准化
         """
         return self.copy(
             include={"name", "description", "parameters", "required"}
@@ -69,6 +69,9 @@ class Function(BaseModel):
                      enum: Optional[tuple] = None,
                      required: bool = False
                      ):
+        """
+        加注属性
+        """
         self.parameters.properties[property_name] = {}
         self.parameters.properties[property_name]['type'] = property_type
         self.parameters.properties[property_name]['description'] = property_description
@@ -77,11 +80,9 @@ class Function(BaseModel):
         if required:
             self.required.append(property_name)
 
-    def parse_schema_to_properties(self, schema: BaseModel):
+    def parse_schema_to_properties(self, schema: Type[BaseModel]):
         """
-        解析pydantic的schema
-        传入一个pydantic的schema，解析成properties
-        参数可以被数据模型所定义
+        解析 pydantic 的 schema
         """
         self.parameters.properties = schema.schema()["properties"]
 
@@ -100,6 +101,13 @@ class File(BaseModel):
     file_url: str = Field(None, description="文件URL")
     caption: str = Field(default='', description="文件注释")
     uploader_uid: str = Field(default=None, description="上传者")
+    bytes: int = Field(default=None)
+    created_at: int = Field(default=int(time.time()))
+
+    @classmethod
+    def create_file(cls, **kwargs):
+        # TODO
+        return cls(**kwargs)
 
     # hash able
     def __eq__(self, other):
@@ -129,6 +137,20 @@ class File(BaseModel):
         return f"[ReadableFile{_comment[:-1]})]"
 
 
+class FunctionCall(BaseModel):
+    name: str
+    arguments: str
+
+
+class ToolCall(BaseModel):
+    """
+    工具调用
+    """
+    id: str = Field(default=None)
+    type: str = Field(default=None)
+    function: FunctionCall = Field(default=None)
+
+
 class Message(BaseModel):
     """
     标准 API 件
@@ -152,13 +174,11 @@ class Message(BaseModel):
 
     meta: Optional[Meta] = Field(default_factory=Meta.default, description="元数据")
 
-    class FunctionCall(BaseModel):
-        name: str
-        arguments: str
-
     function_call: Optional[FunctionCall] = Field(None, description="ai generated function_call")
+    """Deprecated by openai"""
+    tool_calls: Optional[List[ToolCall]] = Field(None, description="tool calls")
+    """a array of ???"""
 
-    """args"""
     role: Literal["system", "assistant", "user", "function"] = "user"
     content: Optional[str] = None
     name: Optional[str] = Field(None, description="speaker_name", regex=r"^[a-zA-Z0-9_]+$")
@@ -172,14 +192,43 @@ class Message(BaseModel):
         # 过滤value中的None
         return {k: v for k, v in values.items() if v is not None}
 
+    def get_functions(self) -> List[FunctionCall]:
+        """
+        获取插件调用函数列表，合并新旧标准。
+        """
+        _calls = [self.function_call]
+        _calls.extend([tools.function for tools in self.tool_calls])
+        # 去None
+        _calls = [_x for _x in _calls if _x]
+        return _calls
+
+    @property
+    def sign_function(self) -> bool:
+        """
+        判断是否有函数需要执行
+        """
+        if self.function_call:
+            return True
+        if self.tool_calls:
+            for tol in self.tool_calls:
+                if tol.function:
+                    return True
+        return False
+
     @property
     def message(self) -> dict:
+        """
+        标准 Openai 消息 dict
+        """
         return self.dict(
             include={"role", "content", "name", "function_call"}
         )
 
     @property
     def request_final(self) -> "Message":
+        """
+        Openai 请求标准格式
+        """
         return self.copy(
             include={"role", "content", "name", "function_call"}
         )
@@ -187,7 +236,7 @@ class Message(BaseModel):
     @property
     def fold(self) -> "Message":
         """
-        用于折叠消息
+        折叠过长特殊类型消息。消息类型为程序自有属性。
         """
         metadata_str = (f"""[FoldDocs](query_id={self.meta.index_id}"""
                         f"""\ntimestamp={self.meta.datatime}"""
@@ -203,10 +252,14 @@ class Message(BaseModel):
     @classmethod
     def create_short_task(cls, task_desc, refer, role: str = None):
         """
-        生成task
+        创建单任务模板
         """
         if not role:
-            role = "[RULE]Please complete the order according to the task description refer to given information, if can't complete, please reply 'give up'"
+            role = (
+                "[RULE]"
+                "Please complete the order according to the task description refer to given information, "
+                "if can't complete, please reply 'give up'"
+            )
         return [
             Message(
                 role="system",
@@ -220,12 +273,15 @@ class Message(BaseModel):
         ]
 
     @classmethod
-    def create_task_message_list(cls, task_desc, refer, role: str = None):
+    def create_task(cls, task_desc, refer, role: str = None):
         """
-        生成task
+        创建任务模板
         """
         if not role:
-            role = "[Scene: chatting in real time]\nPlease complete the order according to the task description refer to given information"
+            role = str(
+                "[Scene: chatting in real time]\n"
+                "Please complete the order according to the task description refer to given information"
+            )
         return [
             Message(
                 role="system",
@@ -248,83 +304,10 @@ class Message(BaseModel):
         ]
 
 
-class TextMessage(Message):
-    """
-    存储类型：普通文本消息
-    """
-
-    class FunctionCall(BaseModel):
-        name: str
-        arguments: str
-
-    message_type = "message"
-
-    ##
-
-    role: Literal["system", "assistant", "user", "function"] = "user"
-    content: Optional[str] = None
-    # speaker
-    name: Optional[str] = Field(None, description="speaker_name", regex=r"^[a-zA-Z0-9_]+$")
-    # AI generated function call
-    function_call: Optional[FunctionCall] = None
-
-    @root_validator
-    def check(cls, values):
-        if values.get("role") == "function" and not values.get("name"):
-            raise ValidationError("sdk param validator:name must be specified when role is function")
-        if not values.get("content") and not values.get("function_call"):
-            raise ValidationError("sdk param validator:content or function_call must be specified")
-        # 过滤value中的None
-        return {k: v for k, v in values.items() if v is not None}
-
-    @property
-    def message(self) -> Message:
-        return Message(
-            role=self.role,
-            name=self.name,
-            content=self.content,
-            function_call=self.function_call
-        )
-
-
-class FileMessage(Message):
-    """
-    多媒体消息
-
-    :param role: 角色
-    :param metadata: 元数据
-
-    """
-    message_type = "file_message"
-    # 参数固定
-    role: Literal["system", "assistant", "user", "function"] = "user"
-    name: Optional[str] = Field("action", description="speaker_name", regex=r"^[a-zA-Z0-9_]+$")
-    content: str = Field(None, description="内容")
-
-    file: File = Field(None, description="文件")
-    caption: str = Field("", description="注释")
-    metadata: Optional[dict] = None
-
-    @root_validator
-    def check(cls, values):
-        if not values.get("file"):
-            raise ValidationError("sdk param validator:file must be specified")
-        return values
-
-    @property
-    def message(self) -> Message:
-        assert hasattr(self.file, "file_name"), "obj not ok"
-        content = f"(Operable File)[{self.file.file_name}]/{self.caption[:20]}.../"
-        # 最终向API发送的消息
-        return Message(
-            role=self.role,
-            name=self.name,
-            content=content,
-            function_call=None
-        )
-
-
 def standardise_for_request(message: Message):
+    """
+    标准化转换，供请求使用
+    """
     if isinstance(message, dict):
         message = Message.parse_obj(message)
     if not isinstance(message, Message):
@@ -336,16 +319,12 @@ def standardise_for_request(message: Message):
 
 
 def parse_message_dict(item: dict):
-    message_type = item.get("message_type", None)
-    if message_type == "message":
-        _message = TextMessage.parse_obj(item)
-    elif message_type == "file_message":
-        _message = FileMessage.parse_obj(item)
+    """
+    将 dict 实例化，用错误hook覆盖
+    """
+    try:
+        _message = Message.parse_obj(item)
+    except Exception as e:
+        logger.exception(f"parse_message_dict:Unknown message type in redis data {e}")
     else:
-        try:
-            _message = Message.parse_obj(item)
-        except Exception as e:
-            logger.error(f"parse_message_dict:Unknown message type in redis data {e}")
-            # raise ValueError(f"Unknown message type in redis data {message_type}")
-        else:
-            return _message
+        return _message
