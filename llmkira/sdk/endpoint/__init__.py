5a5b4319160df6df6da78f69210840b0208d408b
sudoskys <coldlando@hotmail.com>
2023-11-11 23:43:40 +0800

    update

--- llmkira/sdk/endpoint/__init__.py
+++ llmkira/sdk/endpoint/__init__.py
@@ -4,4 +4,11 @@
 # @File    : __init__.py
 # @Software: PyCharm
 
+"""
+这里的 schema 是为服务商的sdk照应而写的。
+包含请求体，响应体，以及对齐的数据结构。
+我们自己的数据结构在 sdk/schema.py 中定义。
+请求时，我们需要将自己的数据结构转换为这里的数据结构。
+"""
 from .tee import Driver
+from .tokenizer import get_tokenizer
