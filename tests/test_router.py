0140839bcdfed86eedbd87721fb342ba288ebe82
sudoskys <coldlando@hotmail.com>
2024-01-17 10:15:43 +0800

    feat(pm2.json):fix

--- tests/test_router.py
+++ tests/test_router.py
@@ -6,31 +6,20 @@
 import sys
 
 sys.path.append("..")
-
-from llmkira.middleware.router import RouterCache, Router
+from llmkira.middleware.router import RouterCache, Router  # noqa: E402
 
 
 def test_schema():
-    r1 = RouterCache(
-        router=[]
-    )
+    r1 = RouterCache(router=[])
     print("\n")
     print(r1)
     try:
-        RouterCache(
-            router=None
-        )
-    except Exception as e:
+        RouterCache(router=None)
+    except Exception:
         pass
     else:
         assert False, "Should Raise Error"
 
-    r3 = Router(
-        from_="from",
-        to_="to",
-        user_id=2200,
-        rules="rule there",
-        method="chat"
-    )
-    print('\n')
+    r3 = Router(from_="from", to_="to", user_id=2200, rules="rule there", method="chat")
+    print("\n")
     print(r3)
