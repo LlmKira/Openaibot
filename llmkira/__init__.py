241086639c90425bd0c5aaf95050c2d161a9249a
sudoskys <coldlando@hotmail.com>
2024-04-16 21:58:57 +0800

    :recycle: refactor(llmkira): update function imports in __init__.py file

--- llmkira/__init__.py
+++ llmkira/__init__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/10/14 下午2:17
 
-from .sdk.func_calling import get_available_plugin_names as get_available_plugin_names
-from .sdk.func_calling import get_loaded_plugins as get_loaded_plugins
-from .sdk.func_calling import get_plugin as get_plugin
-from .sdk.func_calling import load_all_plugins as load_all_plugins
-from .sdk.func_calling import load_builtin_plugin as load_builtin_plugin
-from .sdk.func_calling import load_builtin_plugins as load_builtin_plugins
-from .sdk.func_calling import load_plugin as load_plugin
-from .sdk.func_calling import load_plugins as load_plugins
-from .sdk.func_calling import require as require
+from .sdk.tools import get_available_plugin_names as get_available_plugin_names
+from .sdk.tools import get_entrypoint_plugins as get_entrypoint_plugins
+from .sdk.tools import get_loaded_plugins as get_loaded_plugins
+from .sdk.tools import get_plugin as get_plugin
+from .sdk.tools import load_all_plugins as load_all_plugins
+from .sdk.tools import load_builtin_plugin as load_builtin_plugin
+from .sdk.tools import load_builtin_plugins as load_builtin_plugins
+from .sdk.tools import load_from_entrypoint as load_from_entrypoint
+from .sdk.tools import load_plugin as load_plugin
+from .sdk.tools import load_plugins as load_plugins
+from .sdk.tools import require as require
