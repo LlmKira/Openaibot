a2cd4bb5d224043994f13793f7cc5b9c9443ae20
sudoskys <coldlando@hotmail.com>
2024-04-16 21:55:55 +0800

    rewrite openai lib

---
+++ llmkira/openai/cell.py
@@ -0,0 +1,244 @@
+from abc import ABC
+from typing import Optional, Union, List, Literal, Type
+
+from docstring_parser import parse
+from json_repair import repair_json
+from pydantic import ConfigDict, BaseModel, Field, field_validator, model_validator
+
+
+class FunctionChoice(BaseModel):
+    name: str
+
+    @classmethod
+    def parse(
+        cls,
+        schema_model: Type[BaseModel],
+        plugin_name: str = None,
+    ):
+        """
+        解析 pydantic 的 schema
+        :param schema_model:
+        :param plugin_name:
+        :return:
+        """
+        schema = schema_model.model_json_schema()
+        plugin_name = plugin_name or schema["title"]
+        return cls(
+            name=plugin_name,
+        )
+
+
+class Function(FunctionChoice):
+    class Parameters(BaseModel):
+        type: str = "object"
+        properties: dict = {}
+        required: List[str] = Field(default=[], description="必填参数")
+        model_config = ConfigDict(extra="ignore")
+
+    name: str
+    description: Optional[str]
+    parameters: Parameters = None
+
+    @classmethod
+    def parse(
+        cls,
+        schema_model: Type[BaseModel],
+        plugin_name: str = None,
+    ):
+        """
+        解析 pydantic 的 schema
+        """
+        schema = schema_model.model_json_schema()
+        docstring = parse(schema.__doc__ or "")
+        parameters = {
+            k: v for k, v in schema.items() if k not in ("title", "description")
+        }
+        for param in docstring.params:
+            name = param.arg_name
+            description = param.description
+            if (name in parameters["properties"]) and description:
+                if "description" not in parameters["properties"][name]:
+                    parameters["properties"][name]["description"] = description
+
+        parameters["required"] = sorted(
+            k for k, v in parameters["properties"].items() if "default" not in v
+        )
+
+        if "description" not in schema:
+            if docstring.short_description:
+                schema["description"] = docstring.short_description
+            else:
+                schema["description"] = (
+                    f"Correctly extracted `{cls.__name__}` with all "
+                    f"the required parameters with correct types"
+                )
+        plugin_name = plugin_name or schema["title"]
+        return cls(
+            name=plugin_name,
+            description=schema["description"],
+            parameters=parameters,
+        )
+
+
+class CommonTool(BaseModel):
+    type: Union[Literal["function", "code_interpreter"], str] = "function"
+    model_config = ConfigDict(extra="allow")
+
+
+class ToolChoice(CommonTool):
+    type: Literal["function"] = "function"
+    function: Union[FunctionChoice, Type[BaseModel]]
+
+    @field_validator("function")
+    def check_function(cls, v):
+        if isinstance(v, FunctionChoice):
+            _re = v
+        elif issubclass(v, BaseModel):
+            _re = FunctionChoice.parse(v)
+        else:
+            raise ValueError(
+                "function must be a pydantic model or a FunctionChoice object"
+            )
+        assert isinstance(_re, FunctionChoice), RuntimeError(
+            "function must be a pydantic model or a FunctionChoice object+"
+        )
+        return _re
+
+
+class Tool(ToolChoice):
+    type: Literal["function"] = "function"
+    function: Union[Function, Type[BaseModel]]
+
+    @field_validator("function")
+    def check_function(cls, v):
+        if isinstance(v, Function):
+            _re = v
+        elif issubclass(v, BaseModel):
+            _re = Function.parse(v)
+        else:
+            raise ValueError("function must be a pydantic model or a Function object")
+        assert isinstance(_re, Function), RuntimeError(
+            "function must be a pydantic model or a Function object+"
+        )
+        return _re
+
+
+class FunctionCalled(BaseModel):
+    name: str
+    arguments: Union[dict, str] = None
+
+    @model_validator(mode="after")
+    def check_arguments(self):
+        if isinstance(self.arguments, str):
+            json_repaired = repair_json(self.arguments, return_objects=True)
+            if not json_repaired:
+                self.arguments = {}
+            else:
+                self.arguments = json_repaired
+        return self
+
+
+class ToolCall(BaseModel):
+    id: str
+    type: Literal["function"] = "function"
+    function: FunctionCalled
+
+    @property
+    def name(self):
+        return self.function.name
+
+    @property
+    def arguments(self):
+        return self.function.arguments
+
+
+class Message(BaseModel, ABC):
+    role: Literal["user", "assistant"] = "user"
+    content: str
+
+
+class SystemMessage(Message):
+    role: Literal["system"] = "system"
+    content: str
+    name: str = None
+
+
+class UserMessage(Message):
+    role: Literal["user"] = "user"
+    content: str
+    name: Optional[str] = None
+
+
+class AssistantMessage(Message):
+    role: Literal["assistant"] = "assistant"
+    content: Optional[str] = None
+    name: Optional[str] = None
+    tool_calls: Optional[List[ToolCall]] = None
+
+    @model_validator(mode="after")
+    def check_tool_calls(self):
+        if self.content is None and self.tool_calls is None:
+            raise ValueError("content and tool_calls cannot be both None")
+        return self
+
+
+class ToolMessage(Message):
+    role: Literal["tool"] = "tool"
+    content: str
+    tool_call_id: str
+
+
+def active_cell(message: Union[dict, Message]) -> Optional[Message]:
+    """
+    激活消息
+    :param message: message dict or Message object
+    :return: Message or None
+    :raises ValueError: message is not a valid message dict | message must have a 'role' key
+    :raises NotImplementedError: role is not supported
+    """
+    if isinstance(message, Message):
+        return message
+    elif isinstance(message, dict):
+        if message.get("role") is None:
+            raise ValueError("message must have a 'role' key")
+        if message["role"] == "system":
+            return SystemMessage.model_validate(message)
+        elif message["role"] == "user":
+            return UserMessage.model_validate(message)
+        elif message["role"] == "assistant":
+            return AssistantMessage.model_validate(message)
+        elif message["role"] == "tool":
+            return ToolMessage.model_validate(message)
+        else:
+            raise NotImplementedError(f"role {message.get('role')} is not supported")
+    else:
+        raise ValueError("message is not a valid message dict")
+
+
+def active_cell_string(message: str) -> Optional[Message]:
+    """
+    从字符串中激活消息
+    :param message: message json string
+    :return: Message or None
+    :raises ValueError: message is not a valid message string
+    :raises NotImplementedError: role is not supported
+    """
+    message = repair_json(message, return_objects=True)
+    if not message:
+        return None
+    return active_cell(message)
+
+
+def class_tool(tool: Union[Tool, Type[BaseModel]]) -> Tool:
+    """
+    从类中激活工具
+    :param tool: Tool object
+    :return: tool
+    :raises ValueError: tool is not a valid Tool object
+    """
+    if isinstance(tool, Tool):
+        return tool
+    if issubclass(tool, BaseModel):
+        tool = Tool(function=tool)
+        return tool
+    raise ValueError("tool is not a valid Tool object")
---
+++ llmkira/openai/request.py
@@ -0,0 +1,214 @@
+from typing import List, Union, Type
+from typing import Literal
+from typing import Optional
+
+from curl_cffi.requests import AsyncSession
+from loguru import logger
+from pydantic import ConfigDict, BaseModel, Field, field_validator
+from pydantic import SecretStr
+from tenacity import retry, stop_after_attempt
+
+from llmkira.openai._excption import raise_error, NetworkError, UnexpectedFormatError
+from .cell import Tool, ToolChoice, Message, AssistantMessage, CommonTool, ToolCall
+
+
+class OpenAICredential(BaseModel):
+    api_key: SecretStr
+    base_url: str = "https://api.openai.com/v1"
+    default_headers: dict = {"x-foo": "true"}
+    azure_endpoint: Optional[str] = None
+    api_version: Optional[str] = None
+    model: Optional[str] = None
+    _session: AsyncSession = None
+
+    @property
+    def session(self):
+        return self._session
+
+    @session.setter
+    def session(self, value):
+        self._session = value
+
+    async def get_session(self, timeout: int = 180, update_headers: dict = None):
+        if update_headers is None:
+            update_headers = {}
+        if not self.session:
+            self.session = AsyncSession(
+                timeout=timeout,
+                headers={
+                    "Accept": "*/*",
+                    "Authorization": f"Bearer {self.api_key.get_secret_value()}",
+                    "Content-Type": "application/json",
+                },
+                impersonate="edge101",
+            )
+        self.session.headers.update(update_headers)
+        return self.session
+
+
+class OpenAIResult(BaseModel):
+    class Usage(BaseModel):
+        prompt_tokens: int
+        completion_tokens: int
+        total_tokens: int
+
+    class Choices(BaseModel):
+        index: int
+        message: AssistantMessage
+        finish_reason: str
+        """
+        The reason the model stopped generating tokens. This will be stop if the model hit a natural stop point or
+        a provided stop sequence, length if the maximum number of tokens specified in the request was reached,
+        content_filter if content was omitted due to a flag from our content filters, tool_calls if the model called
+        a tool, or function_call (deprecated) if the model called a function.
+        """
+        delta: dict = None
+        model_config = ConfigDict(arbitrary_types_allowed=True, extra="allow")
+
+        @property
+        def need_action(self):
+            return bool(
+                "function_call" == self.finish_reason
+                or "tool_calls" == self.finish_reason
+            )
+
+    id: str = Field(default="default")
+    model: str
+    # system_fingerprint: str = Field(default=None, alias="system_prompt_fingerprint")
+    choices: List[Choices]
+    usage: Usage
+    model_config = ConfigDict(arbitrary_types_allowed=True, extra="allow")
+
+    @property
+    def default_message(self) -> "AssistantMessage":
+        return self.choices[0].message
+
+
+class OpenAI(BaseModel):
+    class ResponseFormat(BaseModel):
+        type: Literal["json_object", "text"] = "text"
+
+    model: str
+    messages: List[Message]
+
+    @field_validator("messages")
+    def check_messages(cls, v):
+        if not v:
+            raise ValueError("messages cannot be empty")
+        return v
+
+    temperature: Optional[float] = 1
+    n: Optional[int] = 1
+    top_p: Optional[float] = None
+    stop: Optional[Union[str, List[str]]] = Field(None)
+    """Up to 4 sequences where the API will stop generating further tokens."""
+
+    @field_validator("stop")
+    def check_stop(cls, v):
+        if isinstance(v, list):
+            assert (
+                len(v) <= 4
+            ), "Up to 4 sequences where the API will stop generating further tokens."
+        return v
+
+    max_tokens: Optional[int] = None
+    """Max Tokens"""
+    presence_penalty: Optional[float] = None
+    """Presence Penalty"""
+    frequency_penalty: Optional[float] = None
+    """Frequency Penalty"""
+    seed: Optional[int] = None
+    """Seed"""
+    stream: Optional[bool] = False
+    """Stream"""
+    logit_bias: Optional[dict] = None
+    """Logit Bias"""
+    response_format: Optional[ResponseFormat] = None
+    """{ "type": "json_object" } Must be one of text or json_object."""
+    tools: Optional[List[Union[Tool, CommonTool, Type[BaseModel]]]] = None
+    """Tools"""
+
+    @field_validator("tools")
+    def check_tools(cls, v):
+        if v:
+            v = [
+                Tool(function=i)
+                if isinstance(i, BaseModel) and not isinstance(i, Tool)
+                else i
+                for i in v
+            ]
+            assert all(
+                isinstance(i, Tool) for i in v
+            ), "tools must be a list of Tool objects"
+        return v
+
+    tool_choice: Optional[Union["ToolChoice", Literal["auto", "none"]]] = None
+    """Choice"""
+    model_config = ConfigDict(arbitrary_types_allowed=False, extra="allow")
+
+    @staticmethod
+    def make_url(base_url: str):
+        return base_url.strip().rstrip("/") + "/chat/completions"
+
+    @retry(stop=stop_after_attempt(3), reraise=True)
+    async def request(self, session: OpenAICredential) -> OpenAIResult:
+        """
+        :param session: Credential
+        :return: OpenaiResult
+        :raises: NetworkError, UnexpectedError, UnexpectedFormatError
+        """
+        logger.debug(f"Requesting to {self.model}")
+        client = await session.get_session()
+        client: AsyncSession
+        model = session.model if session.model else self.model
+        # Build request
+        kwargs = self.model_dump(
+            exclude_none=True,
+        )
+        kwargs["model"] = model
+        try:
+            response = await client.post(
+                url=self.make_url(session.base_url),
+                headers={
+                    **session.default_headers,
+                    **{"Authorization": f"Bearer {session.api_key.get_secret_value()}"},
+                },
+                json=kwargs,
+            )
+        except Exception as exc:
+            logger.exception(f"Request Error: {exc}")
+            raise NetworkError("Some error occurred while making request.")
+        try:
+            jsoned = response.json()
+        except Exception as exc:
+            logger.exception(f"Server send a invalid response. \n{exc}")
+            raise NetworkError("Server send a invalid response cant be parsed.")
+        if jsoned.get("error"):
+            raise_error(status_code=response.status_code, error_data=jsoned["error"])
+        if response.status_code != 200:
+            _message = jsoned.get("message", "No message details")
+            raise UnexpectedFormatError(
+                f"Unexpected response code {response.status_code} --message {_message}"
+            )
+        try:
+            return OpenAIResult.model_validate(jsoned)
+        except Exception as exc:
+            logger.exception(exc)
+            raise UnexpectedFormatError("Unexpected response format")
+
+    @retry(stop=stop_after_attempt(3), reraise=True)
+    async def extract(
+        self, response_model: Union[Type[BaseModel], Tool], session: OpenAICredential
+    ):
+        self.n = 1
+        self.response_format = None
+        if not isinstance(response_model, Tool):
+            response_model = Tool(function=response_model)
+        self.tools = [response_model]
+        self.tool_choice = ToolChoice(function=response_model.function)
+        result = await self.request(session)
+        try:
+            tool_call = ToolCall.model_validate(result.choices[0].message.tool_calls[0])
+            return response_model.model_validate(tool_call.function.arguments)
+        except Exception:
+            raise RuntimeError("The response model is not matched with the result")
